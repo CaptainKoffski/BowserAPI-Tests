2d34e053fbb20d14f4aac42344884d47912e2931
CaptainKoffski <pit3rbl00d@gmail.com>
2020-11-04 13:46:18 +0300

    Refactor code

--- BowserAPI/bowserapi.py
+++ BowserAPI/bowserapi.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-from flask import Flask, json, request, jsonify, Response, redirect
+from flask import Flask, request, jsonify, redirect
 from flasgger import Swagger
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import glob
-
 
 app = Flask(__name__)
 
@@ -36,16 +35,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bowser.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+
 class World(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     db.UniqueConstraint(name)
     creationdate = db.Column(db.Date, default=datetime.now)
 
+
 class Castle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     worldid = db.Column(db.Integer, db.ForeignKey('world.id'))
     name = db.Column(db.String)
+
 
 class Goomba(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,31 +55,38 @@ class Goomba(db.Model):
     worldid = db.Column(db.Integer, db.ForeignKey('world.id'))
     castleid = db.Column(db.Integer, db.ForeignKey('castle.id'))
 
+
 db.create_all()
 
+
 def load_test_data():
-    for filename in glob.glob('TestData/*.sql'): # loading test data
+    for filename in glob.glob('TestData/*.sql'):  # loading test data
         with open(filename, 'r') as test_data_file:
             query = test_data_file.read()
             db.engine.execute(query)
             db.session.commit()
 
+
 class WorldSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = World
 
+
 class CastleSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Castle
+
 
 class GoombaSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Goomba
         fields = ('id', 'fullname', 'castleid')
 
+
 @app.route('/')
 def root():
     return redirect("/apidocs/", code=302)
+
 
 @app.route('/world', methods=['GET'])
 @app.route('/world/<int:worldid>', methods=['GET'])
@@ -97,7 +106,8 @@ def getworld(worldid=None):
         worlds = World.query.filter_by(id=worldid).all()
     world_schema = WorldSchema(many=True)
     output = world_schema.dump(worlds)
-    return jsonify({ 'world': output })
+    return jsonify({'world': output})
+
 
 @app.route('/world/<int:worldid>', methods=['PUT'])
 def putworld(worldid):
@@ -115,7 +125,8 @@ def putworld(worldid):
     db.session.commit()
     world_schema = WorldSchema(many=False)
     output = world_schema.dump(world)
-    return jsonify({ 'world': output })
+    return jsonify({'world': output})
+
 
 @app.route('/world/<int:worldid>', methods=['DELETE'])
 def deleteworld(worldid):
@@ -132,6 +143,7 @@ def deleteworld(worldid):
     db.session.delete(world)
     db.session.commit()
     return ('', 204)
+
 
 @app.route('/addworld', methods=['POST'])
 def addworld():
@@ -150,7 +162,8 @@ def addworld():
     db.session.commit()
     world_schema = WorldSchema(many=True)
     output = world_schema.dump(World.query.filter_by(name=body_json['name']).all())
-    return jsonify({ 'world' : output })
+    return jsonify({'world': output})
+
 
 @app.route('/world/<int:worldid>/castle', methods=['GET'])
 @app.route('/world/<int:worldid>/castle/<int:castleid>', methods=['GET'])
@@ -170,7 +183,8 @@ def castles(worldid, castleid=None):
         castles = Castle.query.filter_by(worldid=worldid, id=castleid).all()
     castle_schema = CastleSchema(many=True)
     output = castle_schema.dump(castles)
-    return jsonify({ 'castle': output })
+    return jsonify({'castle': output})
+
 
 @app.route('/world/<int:worldid>/castle', methods=['POST'])
 def addcastle(worldid):
@@ -191,6 +205,7 @@ def addcastle(worldid):
     output = castle_schema.dump(Castle.query.filter_by(name=body_json['name']).all())
     return jsonify({'castle': output})
 
+
 @app.route('/world/<int:worldid>/castle/<int:castleid>', methods=['DELETE'])
 def deletecastle(worldid, castleid):
     """
@@ -206,6 +221,7 @@ def deletecastle(worldid, castleid):
     db.session.delete(castle)
     db.session.commit()
     return ('', 204)
+
 
 @app.route('/world/<int:worldid>/goomba', methods=['GET'])
 @app.route('/world/<int:worldid>/goomba/<int:goombaid>', methods=['GET'])
@@ -225,7 +241,8 @@ def goombas(worldid, goombaid=None):
         goombas = Goomba.query.filter_by(worldid=worldid, id=goombaid).all()
     goomba_schema = GoombaSchema(many=True)
     output = goomba_schema.dump(goombas)
-    return jsonify({ 'goomba': output })
+    return jsonify({'goomba': output})
+
 
 @app.route('/world/<int:worldid>/goomba', methods=['POST'])
 def addgoomba(worldid):
@@ -246,6 +263,7 @@ def addgoomba(worldid):
     output = goomba_schema.dump(Goomba.query.filter_by(name=body_json['name']).all())
     return jsonify({'goomba': output})
 
+
 @app.route('/world/<int:worldid>/goomba/<int:goombaid>', methods=['DELETE'])
 def deletegoomba(worldid, goombaid):
     """
@@ -261,6 +279,7 @@ def deletegoomba(worldid, goombaid):
     db.session.delete(goomba)
     db.session.commit()
     return ('', 204)
+
 
 @app.route('/tos')
 def tos():
